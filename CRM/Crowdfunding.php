5e6cdb99fe359b34269ed6cffa842a5e9247514e
John Kirk <accounts@civifirst.com>
2017-05-07 00:38:24 +0000

    Work to a) check that child Contributions are complete before accepting them and b) to update the parent Contribution's status directly in the DB, preventing an error with using the API - it throws an Exception.

--- CRM/Crowdfunding.php
+++ CRM/Crowdfunding.php
@@ -17,11 +17,96 @@ class CRM_Crowdfunding {
    */
   private function refreshParentContributionStatus($parentContributionId) {
 
-    $parentContributionGoal = civicrm_api3('Contribution', 'getvalue', array(
+    // See if the Parent Contribution's total amount is met by the Child Contributions.
+    $parentContributionDetails = civicrm_api3('Contribution', 'getsingle', array(
       'sequential' => 1,
-      'return' => 'total_amount',
+      'return' => array('total_amount', 'contribution_status'),
       'id' => $parentContributionId,
     ));
+
+    $childContributionsTotal = $this->getChildContributionTotal($parentContributionId);
+
+    $newContributionStatus = '';
+    if ($childContributionsTotal <= 0) {
+      // There are no related payments, or the value is negative.
+      $newContributionStatus = 'Pending';
+    }
+    elseif ($childContributionsTotal < $parentContributionDetails['contribution_status']) {
+      // There are payments, but not enough.
+      $newContributionStatus = 'Partially paid';
+    }
+    else {
+      // Paid in full, maybe in excess.
+      $newContributionStatus = 'Completed';
+    }
+
+    if ($newContributionStatus === $parentContributionDetails['contribution_status']) {
+      // The Contribution's status hasn't changed, so updating here would throw the hook unnecessarily.
+      return;
+    }
+
+    // Update the Contribution's status.
+    $this->updateContributionStatus($parentContributionId, $newContributionStatus, $parentContributionDetails['total_amount']); // Bypasses an error with updating contribution statuses where the total amount is needed.
+
+    if ($newContributionStatus === 'Completed') {
+      $this->onParentContributionComplete($parentContributionId);
+    }
+  }
+
+  /**
+   *
+   * @param int $parentContributionId
+   * @param string $newContributionStatus
+   * @param float $totalAmount
+   */
+  private function updateContributionStatus($parentContributionId, $newContributionStatus, $totalAmount) {
+    // TODO when this extension is enabled and the API is called, the API throws
+    // an exception saying that 'total_amount' is required and missing. This is
+    // even when it's present as below. Seemingly only through the interface,
+    // doesn't affect Unit Tests.
+    // civicrm_api3('Contribution', 'create', array(
+    //   'id' => $parentContributionId,
+    //   'contribution_status_id' => $newContributionStatus,
+    //   'total_amount' => $totalAmount,
+    // ));
+
+    $newContributionStatusId = civicrm_api3('OptionValue', 'getvalue', array(
+      'sequential' => 1,
+      'return' => 'value',
+      'name' => $newContributionStatus,
+      'option_group_id' => 'contribution_status',
+    ));
+
+    $updateSql = 'UPDATE civicrm_contribution SET contribution_status_id = %1 WHERE id = %2';
+    $updateParams = array(
+      1 => array($newContributionStatusId, 'Integer'),
+      2 => array($parentContributionId, 'Integer'),
+    );
+
+    CRM_Core_DAO::executeQuery($updateSql, $updateParams);
+  }
+
+  /**
+   *
+   * @param int $parentContributionId
+   * return void
+   */
+  private function onParentContributionComplete($parentContributionId) {
+    // If the Contribution is Completed, update Participants' status if needed.
+    $participantStatuses = CRM_Event_PseudoConstant::participantStatus();
+    $ids = CRM_Event_BAO_Participant::getParticipantIds($parentContributionId);
+    foreach ($ids as $val) {
+      $participantUpdate['id'] = $val;
+      $participantUpdate['status_id'] = array_search('Registered', $participantStatuses);
+      CRM_Event_BAO_Participant::add($participantUpdate);
+    }
+  }
+
+  /**
+   * @param int $parentContributionId
+   * @return int $childContributionsTotal
+   */
+  private function getChildContributionTotal($parentContributionId) {
 
     $childContributions = civicrm_api3('Contribution', 'get', array(
       'sequential' => 1,
@@ -32,44 +117,11 @@ class CRM_Crowdfunding {
     $childContributionsTotal = 0;
 
     foreach ($childContributions['values'] as $childContribution) {
-      if ('Completed' == $childContribution['contribution_status']) {
+      if ('Completed' === $childContribution['contribution_status']) {
         $childContributionsTotal += $childContribution['total_amount'];
       }
     }
-
-    if ($childContributionsTotal <= 0) {
-      // There are no related payments, or the value is negative.
-      civicrm_api3('Contribution', 'create', array(
-        'sequential' => 1,
-        'id' => $parentContributionId,
-        'contribution_status_id' => 'Pending',
-      ));
-    }
-    elseif ($childContributionsTotal < $parentContributionGoal) {
-      // There are payments, but not enough.
-      civicrm_api3('Contribution', 'create', array(
-        'sequential' => 1,
-        'id' => $parentContributionId,
-        'contribution_status_id' => 'Partially paid',
-      ));
-    }
-    else {
-      // Paid in full, maybe in excess.
-      civicrm_api3('Contribution', 'create', array(
-        'sequential' => 1,
-        'id' => $parentContributionId,
-        'contribution_status_id' => 'Completed',
-      ));
-
-      // Update Participants if needed.
-      $participantStatuses = CRM_Event_PseudoConstant::participantStatus();
-      $ids = CRM_Event_BAO_Participant::getParticipantIds($parentContributionId);
-      foreach ($ids as $val) {
-        $participantUpdate['id'] = $val;
-        $participantUpdate['status_id'] = array_search('Registered', $participantStatuses);
-        CRM_Event_BAO_Participant::add($participantUpdate);
-      }
-    }
+    return $childContributionsTotal;
   }
 
   /**
@@ -79,7 +131,7 @@ class CRM_Crowdfunding {
    */
   public function onContributionUpdate($contributeId) {
     $apiParentIdFieldName = self::getApiFieldName();
-    
+
     $parentContributionId = civicrm_api3('Contribution', 'getvalue', array(
       'sequential' => 1,
       'return' => $this->apiParentContributionIdFieldId,
@@ -87,7 +139,7 @@ class CRM_Crowdfunding {
     ));
 
     if (!empty($parentContributionId)) {
-     $this->refreshParentContributionStatus($parentContributionId);
+      $this->refreshParentContributionStatus($parentContributionId);
     }
   }
 
@@ -114,5 +166,5 @@ class CRM_Crowdfunding {
     ));
     return 'custom_' . $parentContributionIdFieldId;
   }
-}
 
+}
