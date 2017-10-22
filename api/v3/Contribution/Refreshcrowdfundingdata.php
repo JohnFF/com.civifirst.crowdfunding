52b03e6a1f0b105d8b0990db7afaf893dfa61d17
John Kirk <accounts@civifirst.com>
2017-10-22 02:25:27 +0000

    Added refresh parent contribution API call.

--- api/v3/Contribution/Refreshcrowdfundingdata.php
+++ api/v3/Contribution/Refreshcrowdfundingdata.php
@@ -10,7 +10,7 @@ use CRM_Crowdfunding_ExtensionUtil as E;
  * @see http://wiki.civicrm.org/confluence/display/CRMDOC/API+Architecture+Standards
  */
 function _civicrm_api3_contribution_Refreshcrowdfundingdata_spec(&$spec) {
-  $spec['magicword']['api.required'] = 1;
+  $spec['contribution']['api.required'] = 1;
 }
 
 /**
@@ -23,20 +23,6 @@ function _civicrm_api3_contribution_Refreshcrowdfundingdata_spec(&$spec) {
  * @throws API_Exception
  */
 function civicrm_api3_contribution_Refreshcrowdfundingdata($params) {
-  if (array_key_exists('magicword', $params) && $params['magicword'] == 'sesame') {
-    $returnValues = array(
-      // OK, return several data rows
-      12 => array('id' => 12, 'name' => 'Twelve'),
-      34 => array('id' => 34, 'name' => 'Thirty four'),
-      56 => array('id' => 56, 'name' => 'Fifty six'),
-    );
-    // ALTERNATIVE: $returnValues = array(); // OK, success
-    // ALTERNATIVE: $returnValues = array("Some value"); // OK, return a single value
-
-    // Spec: civicrm_api3_create_success($values = 1, $params = array(), $entity = NULL, $action = NULL)
-    return civicrm_api3_create_success($returnValues, $params, 'NewEntity', 'NewAction');
-  }
-  else {
-    throw new API_Exception(/*errorMessage*/ 'Everyone knows that the magicword is "sesame"', /*errorCode*/ 1234);
-  }
+  $crowdfunding = new CRM_Crowdfunding();
+  $crowdfunding->refreshParentContributionStatus($params['id']);
 }
